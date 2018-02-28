049913feaf0d56fb6a437f9dd562cfd4515773b2
Andrey Klimenko <a.klimenko@andersenlab.com>
2018-02-28 09:59:06 +0300

    Add test-migration.

---
+++ migrations/m180228_065742_create_doctors_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `doctors`.
+ */
+class m180228_065742_create_doctors_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('doctors', [
+            'id' => $this->primaryKey(),
+            'firstname' => $this->string(255),
+            'lastname' => $this->string(255),
+            'spec' => $this->string(255),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('doctors');
+    }
+}
--- tests/functional/factory/Doctor.php
+++ tests/functional/factory/Doctor.php
@@ -8,11 +8,7 @@
 use League\FactoryMuffin\Faker\Facade as Faker;
 
 $fm->define('Doctor')->setDefinitions([
-    'firstname' => Faker::firstName(),  // Set the firstname attribute to a random male first name
-    'lastname'  => Faker::lastName(),   //
-    'foo'    => Faker::word(),          // Set the foo attribute to a random word
-    'name'   => Faker::firstNameMale(), // Set the name attribute to a random male first name
-    'email'  => Faker::email(),         // Set the email attribute to a random email address
-    'body'   => Faker::text(),          // Set the body attribute to a random string of text
-    'slogan' => Faker::sentence(),      // Set the slogan attribute to a random sentence
+    'firstname' => Faker::firstName(),  // Set the firstname attribute to a random first name
+    'lastname'  => Faker::lastName(),   // Set the lastname attribute to a random last name
+    'spec'    => Faker::jobTitle(),     // Set the spec attribute to a random job
 ]);
